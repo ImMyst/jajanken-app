036beedf26b51d0ec79ae3514aa58a84c6bb2f3d
ImMyst <titouan.galvani@laposte.net>
2018-12-09 00:16:50 +0100

    Game system in console

--- app.js
+++ app.js
@@ -7,3 +7,46 @@ const result = document.querySelector(".result");
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = (Math.floor(Math.random() * 3));
+    return choices[randomNumber];
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            console.log("USER WINS");
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            console.log("USER LOSES");
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            console.log("DRAW");
+            break;
+    }
+}
+
+function main() {
+    rock.addEventListener('click', function () {
+        game("rock");
+    });
+
+    paper.addEventListener('click', function () {
+        game("paper");
+    });
+
+    scissors.addEventListener('click', function () {
+        game("scissors");
+    });
+}
+
+main();
